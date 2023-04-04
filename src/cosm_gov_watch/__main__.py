2c809885b036f53765f4f596c7b8759d6df22320
Ciuciu Mihai <mihai3410@DESKTOP-FH5SU0Q.localdomain>
2023-04-04 17:05:49 +0300

    add changes for slack

--- src/cosm_gov_watch/__main__.py
+++ src/cosm_gov_watch/__main__.py
@@ -1,6 +1,16 @@
 from cosm_gov_watch.api_requests import request_governance
 from config_loader.load_config import load_chains
 from prettytable import PrettyTable
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+import os
+import json
+import requests
+import ssl
+import certifi
+from colorama import Fore, Style
+
+ssl_context = ssl.create_default_context(cafile=certifi.where())
 
 proposals_table = PrettyTable()
 proposals_table.title = f"Chain Proposals"
@@ -10,33 +20,71 @@ proposals_table.border = True
 
 
 def main():
+    SLACK_CHANNEL = "xxxx"
+    client = WebClient(token="xxxx", ssl=ssl_context)
     chains = load_chains()
     for chain in chains["chains"]:
-        proposals_table.add_row([chain["name"],
-                                 "--",
-                                 "--",
-                                 "--",
-                                 "--"])
-        print(f'\rProcessing: {chain["name"]}', end = " ")
-        governance = request_governance(chain["api"])
-        try:
-            for proposal in governance['proposals']:
-                if proposal['status'] == "PROPOSAL_STATUS_VOTING_PERIOD":
-                    proposals_table.add_row([chain["name"],
-                                             proposal['proposal_id'],
-                                             proposal['content']['@type'].rsplit('.', 1)[-1],
-                                             proposal['content']['title'][:60],
-                                             proposal['status']])
-        except KeyError as e:
-            proposals_table.add_row([chain["name"],
-                                     "API error",
-                                     "API error",
-                                     "API error",
-                                     "API error"])
+        # proposals_table.add_row([chain["name"], "--", "--", "--", "--"])
+        # print(f'\x1b[1K\rProcessing: {chain["name"]} -> calling API: {chain["api"]}', end = "")
+        governance = request_governance(chain["api"], chain["version"])
+        #proposal_dict["chain"] = []
+        if chain["version"] == "v1beta1":
+            try:
+                for proposal in governance['proposals']:
+                    if proposal['status'] == "PROPOSAL_STATUS_VOTING_PERIOD":
+                        proposals_table.add_row([chain["name"],
+                                                 proposal['proposal_id'],
+                                                 proposal['content']['@type'].rsplit('.', 1)[-1],
+                                                 proposal['content']['title'][:60],
+                                                 proposal['status']])
+                        client.chat_postMessage(channel="#" + SLACK_CHANNEL, text=chain["name"]+ "  " +\
+                                                proposal['proposal_id']+ "  " +proposal['content']['@type'].rsplit('.', 1)[-1]+"  "+proposal['content']['title'][:60]+"  "+\
+                                                proposal['status']+ "  " +chain["proposal_link"]+proposal['proposal_id'])
+                        #print(f"{Fore.RED}{chain['name']}{Style.RESET_ALL}" +"  " + \
+                        #      proposal['proposal_id'] +"  " + proposal['content']['@type'].rsplit('.', 1)[-1] +"  " +proposal['content']['title'][:60] +"  " + \
+                        #      proposal['status'] + "  " + chain["proposal_link"] + proposal['proposal_id'])
+            except KeyError as e:
+                check_endpoints = True
+                proposals_table.add_row([chain["name"],
+                                         "API error",
+                                         "API error",
+                                         "API error",
+                                         "API error"])
+                client.chat_postMessage(channel="#" + SLACK_CHANNEL, text=f"""
+==========
+{chain["name"].upper()} API error - check api endpoint
+==========""")
 
-    print (f'\rProcessing done. Displaying results.')
-    print(proposals_table)
+        else:
+            try:
+                for proposal in governance['proposals']:
+                    if proposal['status'] == "PROPOSAL_STATUS_VOTING_PERIOD":
+                        proposals_table.add_row([chain["name"],
+                                                 proposal['id'],
+                                                 proposal['messages'][0]['content']['@type'].rsplit('.', 1)[-1],
+                                                 proposal['messages'][0]['content']['title'][:60],
+                                                 proposal['status']])
+                        #client.chat_postMessage(channel="#" + SLACK_CHANNEL, text=chain["name"]+"  "+\
+                        #                        proposal['id']+" "+proposal['messages'][0]['content']['@type'].rsplit('.', 1)[-1]+"  "+proposal['messages'][0]['content']['title'][:60]+"  "+\
+                        #                        proposal['status']+"  " +chain["proposal_link"]+proposal['proposal_id'])
+            except KeyError as e:
+                check_endpoints = True
+                proposals_table.add_row([chain["name"],
+                                         "API error",
+                                         "API error",
+                                         "API error",
+                                         "API error"])
+                client.chat_postMessage(channel="#" + SLACK_CHANNEL, text=f"""
+==========
+{chain["name"].upper()}  API error - check api endpoint
+==========""")
+    #print (f'\rProcessing done. Displaying results.')
+    # print(proposals_table.get_string())
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
--- src/cosm_gov_watch/api_requests.py
+++ src/cosm_gov_watch/api_requests.py
@@ -2,8 +2,11 @@ import json
 import requests
 
 
-def request_governance(api_url):
-    request_url = f"{api_url}/cosmos/gov/v1beta1/proposals"
+def request_governance(api_url, version):
+    if version == "v1beta1":
+        request_url = f"{api_url}/cosmos/gov/v1beta1/proposals?pagination.reverse=true"
+    else:
+        request_url = f"{api_url}/cosmos/gov/v1/proposals?pagination.reverse=true"
     try:
         api_response = requests.get(request_url)
         json_response = json.loads(api_response.text)
